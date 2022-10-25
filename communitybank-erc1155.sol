e0f89af2a9f5efc04bfc201e44083a642a8e1af6
Daniel <103897412+AltESV@users.noreply.github.com>
2022-10-25 14:18:54 +0100

    Update communitybank-erc1155.sol

--- communitybank-erc1155.sol
+++ communitybank-erc1155.sol
@@ -42,35 +42,24 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 
 //edit name of contract
-contract Web3Builders is ERC1155, Ownable, Pausable, ERC1155Supply {
+contract FanaTest is ERC1155, Ownable, Pausable, ERC1155Supply {
 
-
-    //edit max supply of token
     uint256 public maxSupply = 10000;
-
+    uint256 limit = 1 gwei;    
     bool public whiteListActive = false;
-
-    //set limit for whitelisted user withdrawal
-    uint256 limit = 1 ether;
-
-
     mapping (address => bool) public whiteList;
 
-    constructor() ERC1155("ipfs://QmRgj3fm4N5azBVqmHBr3N9WYG4x79nvvGHhGxcUt9Er9r/") {}
+    constructor() ERC1155("ipfs://QmQmuDUFxgBiKXL8NG1UuqHfsn8fYeoYZD4oJtEaSuLXXR/") {}
 
-
-    //allows anyone to deposit to smartcontract
     function deposit(uint256 amount) public payable {
         require(msg.value >0);
         amount = msg.value;
     }
 
-    //allows only owner to set a new withdrawal limit for all whitelisted users
     function setLimit(uint256 _limit) public onlyOwner {
         limit = _limit;
     }
 
-    //swtiches whitelist on or off in case we need to enable / disable access to minting and/or withdrawals
     function setWhiteListActive(bool _whiteListActive) public onlyOwner {
         whiteListActive = _whiteListActive;
     }
@@ -87,27 +76,23 @@ contract Web3Builders is ERC1155, Ownable, Pausable, ERC1155Supply {
         _unpause();
     }
 
-    //allows owner to add addresses to whitelist
     function setWhiteList(address[] calldata addresses) external onlyOwner {
             for (uint256 i = 0; i < addresses.length; i++) {
                 whiteList[addresses[i]] = true;
             }
         }
 
-    //allows owner to remove addresses from whitelist
      function removeFromWhitelist(address[] calldata addresses) external onlyOwner {
         for (uint i = 0; i < addresses.length; i++) {
             delete whiteList[addresses[i]];
         }
     }
 
-    //allows onlyowner to mint unlimited number of token for free up to the max token supply amount into any designated wallet 
     function mint(address account, uint256 id, uint256 amount) public onlyOwner {
         _mint(account, id, amount, "");
         require(totalSupply(id) + amount <= maxSupply, "We have hit the cap.");
     }
 
-    //allows whitelisted to withdraw a specified amount up to a certain limit
     function withdraw(address _addr, uint amount) external {
         require(whiteList[msg.sender], "Address not whitelisted.");
         uint256 balance = address(this).balance;
@@ -116,13 +101,11 @@ contract Web3Builders is ERC1155, Ownable, Pausable, ERC1155Supply {
         payable(_addr).transfer(amount);
     }
 
-    //allows owner to withdraw any amount
     function withdraw(address _addr) external onlyOwner {
         uint256 balance = address(this).balance;
         payable(_addr).transfer(balance);
     }
 
-   //erc1155 boiler plate functions for batch minting, transfer etc.
    function uri(uint256 _id) public view override returns (string memory) {
         require(exists(_id), "URI: nonexistent token");
 
